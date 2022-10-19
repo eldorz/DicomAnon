149904c159b3b5fe13a75b42c27d38e98b579bab
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2022-10-19 16:30:01 +1100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 import shutil
 from PyQt6.QtWidgets import QWidget, QPushButton, QProgressBar, QVBoxLayout, QApplication, QFileDialog, QGroupBox, QLineEdit, QLabel, QGridLayout
 from pydicom import dcmread
-
-patientName = 'Fred Nerk'
+import datetime
+import random
 
 
 class DicomAnonWidget(QWidget):
@@ -51,14 +51,15 @@ class DicomAnonWidget(QWidget):
         self.show()
 
     def anon_button_clicked(self):
-        global patientName
-
-        # make a copy of the patient data
-        patientName = self.givenNameEdit.text() + ' ' + self.familyNameEdit.text()
         # get the file names
         dicom_dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        print(dicom_dir)
+        # get the current time
+        d = datetime.datetime.now()
+        current_date = d.strftime("%Y%m%d")
+        current_time = d.strftime("%H%M%S.%f")
         # get the list of DICOM files in the selected directory
-        dicom_files = glob.glob('{}/*.dcm')
+        dicom_files = glob.glob('{}/**/*.dcm'.format(dicom_dir), recursive=True)
         print('there are {} files'.format(len(dicom_files)))
         if len(dicom_files) > 0:
             # set up the anon directory
@@ -66,10 +67,40 @@ class DicomAnonWidget(QWidget):
             if os.path.exists(ANON_DIR):
                 shutil.rmtree(ANON_DIR)
             os.makedirs(ANON_DIR)
+            # step through the files and replace the identifiable fields in each one
             for f in dicom_files:
                 dicom_filename = os.path.basename(f)
                 ds = dcmread(f)
-                ds.PatientName = 'Fred Nerk'
+                
+                ds.PatientName = '{} {}'.format(self.givenNameEdit.text(), self.familyNameEdit.text())
+                ds.PatientID = self.patientIDEdit.text()
+                patientBirthYear = datetime.datetime.strptime(ds.PatientBirthDate, "%Y%m%d").year
+                ds.PatientBirthDate = datetime.datetime(patientBirthYear, random.randint(1, 12), random.randint(1, 28))
+                ds.PatientBirthTime = current_time
+                ds.PatientSex = 'O'
+                ds.PatientAddress = 'Anonymised'
+                ds.PatientMotherBirthName = 'Anonymised'
+                ds.EthnicGroup = 'Anonymised'
+
+                ds.ReferringPhysicianName = 'Anonymised'
+                ds.ReferringPhysicianAddress = 'Anonymised'
+
+                ds.StudyDescription = 'Anonymised'
+                ds.SeriesDescription = 'Anonymised'
+                ds.StudyID = 'RMIT-EDU'
+
+                ds.StudyDate = current_date
+                ds.StudyTime = current_time
+                ds.SeriesDate = current_date
+                ds.SeriesTime = current_time
+                ds.InstanceCreationDate = current_date
+                ds.InstanceCreationTime = current_time
+                ds.ContentDate = current_date
+                ds.ContentTime = current_time
+                
+                ds.InstitutionName = 'Anonymised'
+                ds.InstitutionAddress = 'Anonymised'
+
                 ds.save_as('{}/{}'.format(ANON_DIR, dicom_filename))
 
 if __name__ == "__main__":
