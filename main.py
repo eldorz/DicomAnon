da84bab63b32b21ae615763de17ffcdaf94b13ec
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2022-10-20 13:28:15 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import glob
 import os
 import shutil
-from PyQt6.QtWidgets import QWidget, QPushButton, QProgressBar, QVBoxLayout, QApplication, QFileDialog, QGroupBox, QLineEdit, QLabel, QGridLayout
+from PyQt6.QtWidgets import QWidget, QPushButton, QProgressBar, QVBoxLayout, QApplication, QFileDialog, QGroupBox, QLineEdit, QLabel, QGridLayout, QErrorMessage, QMessageBox
 from pydicom import dcmread
 import datetime
 import random
@@ -50,58 +50,106 @@ class DicomAnonWidget(QWidget):
         self.setLayout(self.vbox)
         self.show()
 
-    def anon_button_clicked(self):
-        # get the file names
-        dicom_dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
-        print(dicom_dir)
+    def anonymise_image(self, ds):
         # get the current time
         d = datetime.datetime.now()
         current_date = d.strftime("%Y%m%d")
         current_time = d.strftime("%H%M%S.%f")
-        # get the list of DICOM files in the selected directory
-        dicom_files = glob.glob('{}/**/*.dcm'.format(dicom_dir), recursive=True)
-        print('there are {} files'.format(len(dicom_files)))
-        if len(dicom_files) > 0:
-            # set up the anon directory
-            ANON_DIR = dicom_dir + '_ANON'
-            if os.path.exists(ANON_DIR):
-                shutil.rmtree(ANON_DIR)
-            os.makedirs(ANON_DIR)
-            # step through the files and replace the identifiable fields in each one
-            for f in dicom_files:
-                dicom_filename = os.path.basename(f)
-                ds = dcmread(f)
-                
-                ds.PatientName = '{} {}'.format(self.givenNameEdit.text(), self.familyNameEdit.text())
-                ds.PatientID = self.patientIDEdit.text()
-                patientBirthYear = datetime.datetime.strptime(ds.PatientBirthDate, "%Y%m%d").year
-                ds.PatientBirthDate = datetime.datetime(patientBirthYear, random.randint(1, 12), random.randint(1, 28))
-                ds.PatientBirthTime = current_time
-                ds.PatientSex = 'O'
-                ds.PatientAddress = 'Anonymised'
-                ds.PatientMotherBirthName = 'Anonymised'
-                ds.EthnicGroup = 'Anonymised'
 
-                ds.ReferringPhysicianName = 'Anonymised'
-                ds.ReferringPhysicianAddress = 'Anonymised'
+        # update the personal fields
+        ds.PatientName = '{} {}'.format(self.givenNameEdit.text(), self.familyNameEdit.text())
+        ds.PatientID = self.patientIDEdit.text()
+        try:
+            patientBirthYear = datetime.datetime.strptime(ds.PatientBirthDate, "%Y%m%d").year
+        except:
+            patientBirthYear = 1900
+        ds.PatientBirthDate = datetime.datetime(patientBirthYear, random.randint(1, 12), random.randint(1, 28))
+        ds.PatientBirthTime = current_time
+        ds.PatientSex = 'O'
+        ds.PatientAddress = 'Anonymised'
+        ds.PatientMotherBirthName = 'Anonymised'
+        ds.EthnicGroup = 'Anonymised'
+        ds.PatientIdentityRemoved = 'YES'
 
-                ds.StudyDescription = 'Anonymised'
-                ds.SeriesDescription = 'Anonymised'
-                ds.StudyID = 'RMIT-EDU'
+        ds.ReferringPhysicianName = 'Anonymised'
+        ds.ReferringPhysicianAddress = 'Anonymised'
 
-                ds.StudyDate = current_date
-                ds.StudyTime = current_time
-                ds.SeriesDate = current_date
-                ds.SeriesTime = current_time
-                ds.InstanceCreationDate = current_date
-                ds.InstanceCreationTime = current_time
-                ds.ContentDate = current_date
-                ds.ContentTime = current_time
-                
-                ds.InstitutionName = 'Anonymised'
-                ds.InstitutionAddress = 'Anonymised'
+        ds.StudyDescription = 'Anonymised'
+        ds.SeriesDescription = 'Anonymised'
+        ds.StudyID = 'RMIT-EDU'
 
-                ds.save_as('{}/{}'.format(ANON_DIR, dicom_filename))
+        ds.StudyDate = current_date
+        ds.StudyTime = current_time
+        ds.SeriesDate = current_date
+        ds.SeriesTime = current_time
+        ds.InstanceCreationDate = current_date
+        ds.InstanceCreationTime = current_time
+        ds.ContentDate = current_date
+        ds.ContentTime = current_time
+        
+        ds.InstitutionName = 'Anonymised'
+        ds.InstitutionAddress = 'Anonymised'
+
+        # remove private data elements, as there is no guarantee as to what kind of information might be contained in them
+        ds.remove_private_tags()
+
+        return ds
+
+    def valid_input_fields(self):
+        valid = True
+        if len(self.familyNameEdit.text()) == 0:
+            self.familyNameEdit.setStyleSheet("border: 1px solid red;")
+            valid = False
+        if len(self.givenNameEdit.text()) == 0:
+            self.givenNameEdit.setStyleSheet("border: 1px solid red;")
+            valid = False
+        if len(self.patientIDEdit.text()) == 0:
+            self.patientIDEdit.setStyleSheet("border: 1px solid red;")
+            valid = False
+        
+        if not valid:
+            # display a prompt
+            dlg = QMessageBox(self)
+            dlg.setWindowTitle("Missing input fields")
+            dlg.setText("Please enter a value in the fields with a red border.")
+            button = dlg.exec()
+        return valid
+
+    def anon_button_clicked(self):
+        # make sure the fields are not empty
+        if self.valid_input_fields():
+            # get the file names under the directory selected
+            dicom_dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+            # get the list of DICOM files in the selected directory
+            dicom_files = glob.glob('{}/**/*.dcm'.format(dicom_dir), recursive=True)
+            if len(dicom_files) > 0:
+                # set up the anon directory
+                ANON_DIR = dicom_dir + '_ANON'
+                if os.path.exists(ANON_DIR):
+                    shutil.rmtree(ANON_DIR)
+                os.makedirs(ANON_DIR)
+                # step through the files and replace the identifiable fields in each one
+                invalid_file_count = 0
+                for f in dicom_files:
+                    # replicate the directory structure under the anon folder
+                    dirname = os.path.dirname(f)
+                    basename = os.path.basename(f)
+                    anon_dirname = dirname.replace(dicom_dir, ANON_DIR)
+                    output_dicom_filename = '{}/{}'.format(anon_dirname, basename)
+                    # make sure the output file's subdirectory exists
+                    if not os.path.exists(anon_dirname):
+                        os.makedirs(anon_dirname)
+                    # load and process the file
+                    try:
+                        ds = dcmread(f, stop_before_pixels=True)
+                    except Exception as e:
+                        print(e)
+                        invalid_file_count += 1
+                    else:
+                        ds = self.anonymise_image(ds)
+                        ds.save_as(output_dicom_filename)
+
+                print('there were {} invalid files ({} files in total)'.format(invalid_file_count, len(dicom_files)))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
