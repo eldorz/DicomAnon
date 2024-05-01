2c9b0b2791062583fd535f30428b26062d003e41
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2024-05-01 15:53:19 +1000

    Update DicomAnon.py

--- DicomAnon.py
+++ DicomAnon.py
@@ -1,12 +1,12 @@
 import sys
 import glob
 import os
-import shutil
-from PyQt6.QtWidgets import QWidget, QPushButton, QProgressBar, QVBoxLayout, QHBoxLayout, QApplication, QFileDialog, QTextEdit, QLabel
+from PyQt6.QtWidgets import QWidget, QPushButton, QProgressBar, QVBoxLayout, QHBoxLayout, QApplication, QFileDialog, QLabel, QMessageBox
 from PyQt6.QtCore import Qt
 from pydicom import dcmread
-import datetime
-import random
+import pandas as pd
+from os.path import expanduser
+from datetime import datetime
 
 StyleSheet = '''
 #BlueProgressBar {
@@ -48,14 +48,7 @@ class DicomAnonWidget(QWidget):
         # progress bar
         self.pbar = QProgressBar(self, minimum=0, maximum=100, textVisible=False, objectName="BlueProgressBar")
         self.pbar.setValue(0)
-        self.pbar.setVisible(False)
-
-        # log output
-        self.logOutput = QTextEdit(self)
-        self.logOutput.setReadOnly(True)
-        self.logFont = self.logOutput.font()
-        self.logFont.setFamily("Courier")
-        self.logFont.setPointSize(10)
+        self.pbar.setVisible(True)
 
         # source folder
         self.source_hbox = QHBoxLayout()
@@ -67,17 +60,18 @@ class DicomAnonWidget(QWidget):
         self.destination_hbox.addWidget(self.destination_button)
         self.destination_hbox.addWidget(self.destination_label)
 
-        self.resize(600, 400)
+        self.setFixedWidth(600)
         self.vbox = QVBoxLayout()
         self.vbox.setSpacing(5)
-        self.vbox.addWidget(self.pbar)
         self.vbox.addLayout(self.source_hbox)
         self.vbox.addLayout(self.destination_hbox)
         self.vbox.addWidget(self.anon_button)
-        self.vbox.addSpacing(10)
-        self.vbox.addWidget(self.logOutput)
+        self.vbox.setSpacing(10)
+        self.vbox.addWidget(self.pbar)
         self.setLayout(self.vbox)
         self.show()
+        self.activateWindow()
+        self.raise_()
 
     def anonymise_image(self, image, name):
         # update the personal fields
@@ -102,85 +96,107 @@ class DicomAnonWidget(QWidget):
 
         return image
 
-    # look for existing anon folders and determine the next ID to use
-    def get_base_id(self, destination_dir):
-        last_id = None
-        dir_names_l = sorted(glob.glob('{}/Brain-???'.format(destination_dir), recursive=False))
-        if len(dir_names_l) > 0:
-            last_name = dir_names_l[-1]
-            last_id = int(last_name.split('Brain-')[1])
+    # look at the mapping file and determine the next ID to use
+    def get_anon_patient_id(self, patient_id, mapping_df):
+        new_patient = False
+        if mapping_df is None:
+            print('no previous mapping - starting')
+            anon_patient_id = 1
+            new_patient = True
         else:
-            last_id = 1
-        return last_id
+            # have we seen this patient ID before?
+            row_df = mapping_df[mapping_df.patient_id == patient_id]
+            if len(row_df) > 0:
+                anon_patient_id = row_df.iloc[0].anon_patient_id
+                new_patient = False
+                print('patient ID {} seen previously (anon ID {}) - appending to existing directory'.format(patient_id, anon_patient_id))
+            else:
+                anon_patient_id = mapping_df.anon_patient_id.max()+1
+                new_patient = True
+                print('patient ID {} not seen previously - adding new anon ID {} directory'.format(patient_id, anon_patient_id))
+        return new_patient, anon_patient_id
 
     # process all DICOMs under the selected top-level folder containing patient folders
-    def process_folder(self, source_dir, destination_dir):
-        # directories under the top-level folder
-        base_dir = os.path.dirname(source_dir)
-        source_subdir_l = glob.glob('{}/*'.format(source_dir), recursive=False)
-
-        print('checking {}'.format(source_dir))
-        patient_id = self.get_base_id(destination_dir)
-        print('base patient ID: {}'.format(patient_id))
-        patients_d = {}
-        for tl_file_idx,source_subdir in enumerate(source_subdir_l):
-            if os.path.isdir(source_subdir):
-                # this is a patient folder
-                patient_folder_name = os.path.basename(source_subdir)
-                print('processing {}'.format(patient_folder_name))
-                folder_sub = source_subdir + os.sep + patient_folder_name
-                # the anon patient folder
-                anon_patient_folder_name = 'Brain-{:03d}'.format(patient_id)
-                # to anonymise the folder names, we now have the substring to replace with
-                anon_folder_sub = self.destination_dir + os.sep + anon_patient_folder_name
-                # now find the DICOMs under the patient folder
-                dicom_files = glob.glob('{}/**/*.dcm'.format(source_subdir), recursive=True)
-                invalid_file_count = 0
-                for dicom_fn in dicom_files:
-                    anon_dicom_fn = base_dir + os.sep + dicom_fn.replace(folder_sub, anon_folder_sub)
-                    anon_dicom_fn_dirname = os.path.dirname(anon_dicom_fn)
+    def process_folder(self, source_base_dir, destination_base_dir, mapping_df):
+        # count the DICOM files under the selected directory
+        dicom_files_count = len(glob.glob('{}/**/*.dcm'.format(source_base_dir), recursive=True))
+        print('{} files'.format(dicom_files_count))
+        dicom_files_processed = 0
+        # find the patient directories
+        patient_dirs_l = [ name for name in os.listdir(source_base_dir) if os.path.isdir(os.path.join(source_base_dir, name)) ]
+        for patient_dir_idx,patient_dir in enumerate(patient_dirs_l):
+            invalid_file_count = 0
+            patient_id = int(patient_dir.split('_')[0])
+            new_patient, anon_patient_id = self.get_anon_patient_id(patient_id, mapping_df)
+            anon_patient_folder_name = 'Brain-{:04d}'.format(anon_patient_id)
+            anon_patient_dir = destination_base_dir + os.sep + anon_patient_folder_name
+            patient_full_dir = source_base_dir + os.sep + patient_dir
+            dicom_files = glob.glob('{}/**/*.dcm'.format(patient_full_dir), recursive=True)
+            for source_file in dicom_files:
+                rel_path = os.path.relpath(source_file, patient_full_dir)  # use the same relative path for source and target
+                anon_patient_file = anon_patient_dir + os.sep + rel_path   # add the relative path to the 'Brain-nnnn' directory
+                # load and process the file
+                try:
+                    ds = dcmread(source_file)
+                except Exception as e:
+                    print(e)
+                    invalid_file_count += 1
+                else:
                     # create the anon folder if it doesn't exist
-                    if not os.path.exists(anon_dicom_fn_dirname):
-                        os.makedirs(anon_dicom_fn_dirname)
-                    # load and process the file
-                    try:
-                        ds = dcmread(dicom_fn)
-                    except Exception as e:
-                        print(e)
-                        invalid_file_count += 1
-                    else:
-                        # ds = self.anonymise_image(image=ds, name=anon_patient_folder_name)
-                        ds.save_as(anon_dicom_fn)
-                patients_d[patient_id] = {'number_of_files':len(dicom_files), 
-                                          'invalid_file_count':invalid_file_count,
-                                          'from_folder':tl_file,
-                                          'to_folder':anon_tl_path
-                                          }
-                patient_id += 1
+                    target_dir = os.path.dirname(anon_patient_file)  # create the missing directories all the way to the DICOM file
+                    if not os.path.exists(target_dir):
+                        os.makedirs(target_dir)
+                    ds.save_as(anon_patient_file)
+                # update count of files processed
+                dicom_files_processed += 1
+                # update the progress bar
+                proportion_completed = int((dicom_files_processed)/dicom_files_count*100)
+                self.pbar.setValue(proportion_completed)
+                # process GUI events to reflect the update value
+                QApplication.processEvents()
+            # count the total sessions anonymised for this patient
+            anon_patient_sessions_l = [ name for name in os.listdir(anon_patient_dir) if os.path.isdir(os.path.join(anon_patient_dir, name)) ]
+            session_count = len(anon_patient_sessions_l)
+            # add or update the mapping
+            date_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            if new_patient:
+                row = pd.Series({'patient_id':patient_id, 'anon_patient_dir_name':anon_patient_folder_name, 'anon_patient_id':anon_patient_id, 'total_session_count':session_count, 'invalid_file_count':invalid_file_count, 'last_updated':date_str})
+                mapping_df = pd.concat([mapping_df, pd.DataFrame([row], columns=row.index)]).reset_index(drop=True)
+            else:
+                row_index = mapping_df.loc[mapping_df['patient_id'] == patient_id].index[0]
+                mapping_df.loc[row_index, 'total_session_count'] = session_count
+                mapping_df.loc[row_index, 'last_updated'] = date_str
+                mapping_df.loc[row_index, 'invalid_file_count'] += invalid_file_count
 
-            # update the progress bar
-            proportion_completed = int((tl_file_idx+1)/len(tl_files_l)*100)
-            self.pbar.setValue(proportion_completed)
-            # process GUI events to reflect the update value
-            QApplication.processEvents()
-        return patients_d
+        return mapping_df
 
     def anon_button_clicked(self):
         # get the file names under the directory selected
-        if self.dicom_dir != "":
+        if self.source_dir != "":
             # update the progress bar
             self.pbar.setValue(0)
             self.pbar.setVisible(True)
+            QApplication.processEvents()
+            # disable buttons
+            self.source_button.setEnabled(False)
+            self.destination_button.setEnabled(False)
+            self.anon_button.setEnabled(False)
+            # set up the mapping file
+            mapping_file = '{}dicom-anon-mapping.xlsx'.format(expanduser('~')+os.sep)
+            if os.path.isfile(mapping_file):
+                mapping_df = pd.read_excel(mapping_file, index_col=0)
+            else:
+                mapping_df = None
             # process the DICOMs within
-            patients_d = self.process_folder(self.dicom_dir)
-            # display log
-            self.logOutput.setCurrentFont(self.logFont)
-            # self.logOutput.setTextColor(color)
-            for k, v in patients_d.items():
-                log_msg = 'Patient {}\nsource: {}\ndestination: {}\n{} files processed\n{} invalid\n------\n'.format(k, v['from_folder'], v['to_folder'], v['number_of_files'], v['invalid_file_count'])
-                self.logOutput.insertPlainText(log_msg)
-                sb = self.logOutput.verticalScrollBar()
-                sb.setValue(sb.maximum())
+            mapping_df = self.process_folder(self.source_dir, self.destination_dir, mapping_df)
+            # update the mapping file
+            mapping_df.to_excel(mapping_file)
+            # enable buttons
+            self.source_button.setEnabled(True)
+            self.destination_button.setEnabled(True)
+            self.anon_button.setEnabled(True)
+            # show that we've finished
+            QMessageBox.information(self, 'DicomAnon', 'Processing has completed.')
 
     def source_button_clicked(self):
         self.source_dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
